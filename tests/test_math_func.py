c08eab07106aacca03be2bc84e3baf5d8414bdf9
migueis <migueis@hotmail.com>
2022-06-12 15:22:56 -0500

    improve

--- tests/test_math_func.py
+++ tests/test_math_func.py
@@ -4,7 +4,7 @@ import pytest
 
 # @pytest.mark.number
 def test_add():
-    assert math_func.add(7, 3) == 9
+    assert math_func.add(7, 3) == 10
     assert math_func.add(7) == 9
 
 # @pytest.mark.number
